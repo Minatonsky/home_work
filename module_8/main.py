e8bd6ec142ace56a92a3613f0b7d394bced21f68
Denys <denkuryshko@gmail.com>
2023-12-10 13:56:29 +0200

    fixed homework 8

--- module_8/main.py
+++ module_8/main.py
@@ -1,25 +1,20 @@
-from datetime import date, timedelta, datetime
-
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
-today = date.today()
-
+from datetime import date, timedelta
 def close_birthday_users(users, start, end):
+    now = date.today()
     result = []
     for user in users:
-        birthday = user.get('birthday').replace(year=today.year)
-        if birthday < today:
-            birthday = birthday.replace(year=today.year + 1)
-        if today <= birthday <= end:
+        birthday = user.get('birthday').replace(year=now.year)
+        if birthday < now:
+            birthday = birthday.replace(year=now.year + 1)
+        if now <= birthday <= end:
             result.append(user)
     return result
 def get_birthdays_per_week(users):
-
+    WEEKDAYS = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',)
     now = date.today()
-
+    current_week_day = now.weekday()
     if not users:
         return {}
-
     start_date = now
     end_date = now + timedelta(days=7)
     birthday_users = close_birthday_users(users, start=start_date, end=end_date)
@@ -30,9 +25,9 @@ def get_birthdays_per_week(users):
             user_birthday = user_birthday.replace(year=now.year + 1)
         user_birthday_weekday = user_birthday.weekday()
         try:
-            user_happy_day = weekdays[user_birthday_weekday]
+            user_happy_day = WEEKDAYS[user_birthday_weekday]
         except IndexError:
-            user_happy_day = weekdays[0]
+            user_happy_day = WEEKDAYS[0]
         if user_happy_day not in result_dict:
             result_dict[user_happy_day] = []
         result_dict[user_happy_day].append(user.get('name'))
@@ -40,11 +35,15 @@ def get_birthdays_per_week(users):
 
 if __name__ == "__main__":
     users = [
-        {"name": "Denys Kuryshko", "birthday": datetime(2023, 12, 9).date()},
-        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
-        {"name": "Bill Gates", "birthday": datetime(2023, 12, 2).date()},
-        {"name": "Jan Koum", "birthday": datetime(2023, 12, 5).date()},
-        {"name": "Iryna Popovych", "birthday": datetime(2023, 12, 30).date()}
+        {
+            "name": "John",
+            "birthday": (date.today() + timedelta(days=1)),
+        },
+        {
+            "name": "Doe",
+            "birthday": (date.today() + timedelta(days=3)),
+        },
+        {"name": "Alice", "birthday": (date.today() + timedelta(days=-3))},
     ]
 
     result = get_birthdays_per_week(users)
